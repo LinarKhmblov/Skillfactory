6ec07b937fbe4dfd2ccfd2fb9bd7e1ffac1c2fe3
LinarKhmblov <125352376+LinarKhmblov@users.noreply.github.com>
2023-04-28 08:54:29 +0300

    Add files via upload

---
+++ 15.3 Работа с JSON файлами.py
@@ -0,0 +1,7 @@
+# import json # простые шаги импорта json файла
+#
+# with open('json_example.json', encoding='utf8') as f:
+#     templates = json.load(f)
+#
+# print(templates)
+# print(type(templates))
